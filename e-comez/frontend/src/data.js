ad0707f5e50c675bfe1ce9f0108ea8f90d13a332
GiDiCoder <frgideon100@gmail.com>
2022-01-15 18:31:26 +0000

    share code on github

---
+++ e-comez/frontend/src/data.js
@@ -0,0 +1,92 @@
+const data = {
+    products: [{
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p17.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p52.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.1,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Lacoste  Shirt',
+            category: 'Shirts',
+            image: '/images/p49.jpg',
+            price: 190,
+            brand: 'Lacoste',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p48.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.4,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p51.jpg',
+            price: 93,
+            brand: 'Puma',
+            rating: 4.4,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: '/images/p52.jpg',
+            price: 76,
+            brand: 'Adidas',
+            rating: 4.3,
+            numReviews: 12,
+            description: 'high quality product'
+        },
+        {
+            _id: '7',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/p38.jpg',
+            price: 76,
+            brand: 'Gucci',
+            rating: 4.4,
+            numReviews: 12,
+            description: 'high quality product'
+        },
+        {
+            _id: '8',
+            name: 'Fila Shirt',
+            category: 'Shirts',
+            image: '/images/p42.jpg',
+            price: 76,
+            brand: 'Fila',
+            rating: 4.3,
+            numReviews: 11,
+            description: 'high quality product'
+        },
+    ],
+}
+export default data;
